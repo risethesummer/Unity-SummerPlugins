61f5e02d8c5289e06ec4c351add0f1d3cc1ff38a
risethesummer <honhatlinhmail@gmail.com>
2023-12-08 17:25:13 +0700

    init

---
+++ Assets/Samples/VContainer/SamplePresenter.cs
@@ -0,0 +1,20 @@
+﻿using VContainer.Unity;
+
+namespace Samples.VContainer
+{
+    public class SamplePresenter : IStartable
+    {
+        private readonly SampleService _service;
+        private readonly SampleView _view;
+
+        public SamplePresenter(SampleService service, SampleView view)
+        {
+            _service = service;
+            _view = view;
+        }
+        public void Start()
+        {
+            _view.Btn.onClick.AddListener(_service.SampleDo);
+        }
+    }
+}
---
+++ Assets/Samples/VContainer/SampleView.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Samples.VContainer
+{
+    public class SampleView : MonoBehaviour
+    {
+        [field: SerializeField] public Button Btn { get; private set; }
+    }
+}
